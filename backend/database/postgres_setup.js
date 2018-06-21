b733f5b4567934c4b6bb01bf8ad581942b174e4d
Guillaume Pitot <guillaume.pitot@gmail.com>
2018-06-21 21:58:16 +1000

    rest of tables

--- backend/database/postgres_setup.js
+++ backend/database/postgres_setup.js
@@ -65,6 +65,52 @@ CREATE TABLE IF NOT EXISTS SHOWS (
 
 
 
+/* --------------------------------------------------
+-------------------- TAGS TRACKS RESIDENTS -------------------------
+---------------------------------------------------*/
+let track_table = `
+CREATE TABLE IF NOT EXISTS TRACKS (
+	track_id SERIAL PRIMARY KEY,
+	show_id INTEGER REFERENCES SHOWS(show_id),
+	title VARCHAR(100),
+	artist VARCHAR(100)
+)
+`;
+
+let tag_table = `
+CREATE TABLE IF NOT EXISTS TAGS (
+	tag_id SERIAL PRIMARY KEY,
+	show_id INTEGER REFERENCES SHOWS(show_id),
+	tag VARCHAR(25)
+);
+`;
+
+
+/* --------------------------------------------------
+-------------------- RESIDENTS -------------------------
+---------------------------------------------------*/
+let res_table = `
+CREATE TABLE IF NOT EXISTS RESIDENTS (
+	resident_id SERIAL PRIMARY KEY ,
+	name VARCHAR(100),
+	description VARCHAR(100),
+	image_thumbnail VARCHAR(100) REFERENCES IMAGES (image_id),
+	image_banner VARCHAR(100) REFERENCES IMAGES (image_id),
+	guest BOOLEAN DEFAULT FALSE
+);
+`; 
+
+
+let res_shows_table = `
+CREATE TABLE IF NOT EXISTS SHOW_RESIDENT_RELATIONSHIPS (
+	show_resident_id SERIAL PRIMARY KEY,
+	show_id INTEGER REFERENCES SHOWS(show_id),
+	resident_id INTEGER REFERENCES RESIDENTS(resident_id)
+);
+`
+
+
+
 
 
 
@@ -72,18 +118,25 @@ function add_table() {
 	pool.connect((err, client, done) => {
 		if (err) throw err;
 
-		client.query(banner_table, (err, res)=> {
+		client.query(track_table, (err, res)=> {
 			done();
 			if (err) console.log(err);
 		})
-		client.query(events_table, (err, res)=> {
+		client.query(tag_table, (err, res)=> {
 			done();
 			if (err) console.log(err);
 		})
-		client.query(show_table, (err, res)=> {
+		client.query(res_table, (err, res)=> {
 			done();
 			if (err) console.log(err);
 		})
+
+		client.query(res_shows_table, (err, res)=> {
+			done();
+			if (err) console.log(err);
+		})
+
+
 	})
 }
 
