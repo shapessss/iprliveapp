55e29603309e14f693b238699bdd544370c5fb9f
Guillaume Pitot <guillaume.pitot@gmail.com>
2018-06-21 21:51:55 +1000

    banner events show added

--- backend/database/postgres_setup.js
+++ backend/database/postgres_setup.js
@@ -8,7 +8,9 @@ const pool = new Pool({
 
 
 
-
+/* --------------------------------------------------
+-------------------- IMAGES -------------------------
+---------------------------------------------------*/
 let image_sql = `
 CREATE TABLE IMAGES (
 	image_id VARCHAR(100) PRIMARY KEY,
@@ -17,7 +19,49 @@ CREATE TABLE IMAGES (
 `;
 
 
+/* --------------------------------------------------
+-------------------- BANNERS -------------------------
+---------------------------------------------------*/
+let banner_table = `
+CREATE TABLE BANNERS (
+	banner_id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	description VARCHAR(100),
+	image_banner VARCHAR(100) REFERENCES IMAGES (image_id),
+	url VARCHAR(100)
+)`;
 
+/* --------------------------------------------------
+-------------------- EVENTS -------------------------
+---------------------------------------------------*/
+let events_table = `
+CREATE TABLE IF NOT EXISTS EVENTS (
+	event_id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	image_thumbnail VARCHAR(100) REFERENCES IMAGES (image_id),
+	date DATE,
+	url VARCHAR(100)
+);
+`
+
+
+
+/* --------------------------------------------------
+-------------------- SHOWS -------------------------
+---------------------------------------------------*/
+let show_table = `
+CREATE TABLE IF NOT EXISTS SHOWS (
+	show_id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	description VARCHAR(100),
+	image_thumbnail VARCHAR(100) REFERENCES IMAGES (image_id),
+	image_banner VARCHAR(100) REFERENCES IMAGES (image_id),
+	date DATE,
+	frequency VARCHAR(20),
+	featured BOOLEAN DEFAULT FALSE,
+	stream VARCHAR(200)
+);
+`;
 
 
 
@@ -28,8 +72,17 @@ function add_table() {
 	pool.connect((err, client, done) => {
 		if (err) throw err;
 
-		client.query(image_sql, (err, res)=> {
+		client.query(banner_table, (err, res)=> {
 			done();
+			if (err) console.log(err);
+		})
+		client.query(events_table, (err, res)=> {
+			done();
+			if (err) console.log(err);
+		})
+		client.query(show_table, (err, res)=> {
+			done();
+			if (err) console.log(err);
 		})
 	})
 }
